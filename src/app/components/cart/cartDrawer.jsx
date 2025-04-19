f63818986cffb4e0df231b54f15163eb4ebbb54b
margaretmini <dominhchau2405@gmail.com>
2025-04-19 19:54:07 +0700

    1st commit

--- src/app/components/cart/cartDrawer.jsx
+++ src/app/components/cart/cartDrawer.jsx
@@ -23,10 +23,16 @@ const CartDrawer = () => {
   const fetchWishlist = async () => {
     try {
       const response = await wishlistApi.wishList();
-      const data = response.data?.data?.content || [];
-      setWishlist(data);
+      const data = response?.data?.data;
+
+      if (Array.isArray(data)) {
+        setWishlist(data);
+      } else {
+        setWishlist([]);
+      }
     } catch (error) {
       console.error("Lỗi khi lấy wishlist:", error);
+      setWishlist([]);
     }
   };
 
@@ -48,29 +54,28 @@ const CartDrawer = () => {
           {wishlist.length === 0 ? (
             <p className="text-white">Chưa có sản phẩm yêu thích nào.</p>
           ) : (
-            wishlist.map((item) => {
-              const { equipment } = item;
-              return (
-                <div
-                  className="flex flex-row justify-between items-center bg-white rounded overflow-hidden pl-4 pr-2"
-                  key={item.id}
-                >
-                  <img
-                    src={equipment?.image || "https://via.placeholder.com/120"}
-                    className="w-[120px] h-[100px] object-cover"
-                    alt={equipment.model}
-                  />
-                  <div>
-                    <p className="text-black w-[170px] font-semibold line-clamp-2">
-                      {equipment.model}
-                    </p>
-                    <p className="text-[#0B0B0B] w-[170px] pt-2">
-                      Giá thuê: {equipment.dailyRate?.toLocaleString()}đ/ngày
-                    </p>
-                  </div>
+            wishlist.map((item) => (
+              <div
+                className="flex flex-row justify-between items-center bg-white rounded overflow-hidden pl-4 pr-2"
+                key={item.equipmentId}
+              >
+                <img
+                  src={
+                    item.primaryImageUrl || "https://via.placeholder.com/120"
+                  }
+                  className="w-[120px] h-[100px] object-cover"
+                  alt={item.model || "Không có tên"}
+                />
+                <div>
+                  <p className="text-black w-[170px] font-semibold line-clamp-2">
+                    {item.model || "Không rõ thiết bị"}
+                  </p>
+                  <p className="text-[#0B0B0B] w-[170px] pt-2">
+                    Giá thuê: {item.dailyRate?.toLocaleString() || "N/A"}đ/ngày
+                  </p>
                 </div>
-              );
-            })
+              </div>
+            ))
           )}
         </div>
       </Drawer>
